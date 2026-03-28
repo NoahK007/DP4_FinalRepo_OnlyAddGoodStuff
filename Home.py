c674c152afe2b1bd1b365d29e527095c12393eb6
asheezah <aishazahid5@gmail.com>
2026-03-28 02:04:45 +0000

    Last commit of tonight (I think) - I just put Dr McDonald's contact page and Home.py as functions, and also added a feedback dialog box on Home.py, plus some balloons for fun

--- Home.py
+++ Home.py
@@ -1,34 +1,52 @@
 import streamlit as st
 
-st.set_page_config(initial_sidebar_state="expanded", page_title="Maccessible")
+#Feedback Dialog
+@st.dialog("Send Feedback", width = "medium", dismissible = True, icon = '📃')
+def give_feedback():
+    selected = st.feedback("stars")
+    fb = st.text_input("Share your feedback below:")
+    if st.button("Submit Feedback"):
+        st.rerun()
 
-st.markdown("""
-    <style>
-        [data-testid="stSidebarNav"] {display: none;}
-    </style>
-""", unsafe_allow_html=True)
+def home_page():
+    
+    st.set_page_config(initial_sidebar_state="expanded", page_title="Maccessible")
 
-st.title("**MACCESSIBLE**  :sunglasses:!", width = "stretch", text_alignment = "center")
-st.markdown("Your all-in-one place for accessibility mapping, currently available only at McMaster University.")
+    #Remove the extra page things at the top that are unnecessary and
+    #unfortunately don't have emojis
+    st.markdown("""
+        <style>
+            [data-testid="stSidebarNav"] {display: none;}
+        </style>
+    """, unsafe_allow_html=True)
 
-#Setting Sidebar to have linked pages (i.e. like a menu) (not buttons)
-##
-st.sidebar.subheader("Menu")
-st.sidebar.page_link("Home.py", label = "Home", icon = '🏠')
-st.sidebar.page_link("pages/Map.py", label = "Map", icon = '🚗')
-st.sidebar.page_link("pages/Help.py", label = "Help!", icon = '⁉️')
-st.sidebar.page_link("pages/About Us.py", label = "About Us...", icon = '✨')
-st.sidebar.page_link("pages/FAQs.py", label = "FAQs", icon = '❓')
-st.sidebar.page_link("pages/Dr McDonald's Contact Info.py", label =
-                     "Dr McDonald's Contact Info", icon = '💯')
+    st.title("**MACCESSIBLE**  :sunglasses:!", width = "stretch", text_alignment = "center")
+    st.space("xsmall")
+    st.markdown("Your all-in-one place for accessibility mapping, currently available only at McMaster University.",
+                text_alignment = "center")
 
-data_from = st.text_input("Enter current location:")
-data = st.columns(2)
-#data_to, to_map = st.columns(2)
-data_to = data[0].text_input("To where?")
-data[1].space("small")
-to_map = data[1].button("Take me to Map!", icon = '➡️', icon_position = 'left')
-if to_map:
-    st.switch_page("pages/Map.py")
-elif to_map and data_to != None and data_from != None:
-    st.switch_page("pages/Map.py")
+    #Setting Sidebar to have linked pages (i.e. like a menu) (not buttons)
+    ##
+    st.sidebar.subheader("Menu")
+    st.sidebar.page_link("Home.py", label = "Home", icon = '🏠')
+    st.sidebar.page_link("pages/Map.py", label = "Map", icon = '🚗')
+    st.sidebar.page_link("pages/Help.py", label = "Help!", icon = '⁉️')
+    st.sidebar.page_link("pages/About Us.py", label = "About Us...", icon = '✨')
+    st.sidebar.page_link("pages/FAQs.py", label = "FAQs", icon = '❓')
+    st.sidebar.page_link("pages/Dr McDonald's Contact Info.py", label =
+                         "Dr McDonald's Contact Info", icon = '💯')
+
+    st.space("small")
+    st.balloons()
+    to_map = st.button("Take me to Map!", icon = '➡️', width="stretch")
+    if to_map:
+        st.switch_page("pages/Map.py")
+    elif to_map and data_to != None and data_from != None:
+        st.switch_page("pages/Map.py")
+    #Feedback Form
+    st.space("xsmall")    
+    feedback = st.button(":bar_chart: Looking to give feedback?", width = "stretch")
+    if feedback:
+        give_feedback()
+            
+home_page()
--- pages/Dr McDonald's Contact Info.py
+++ pages/Dr McDonald's Contact Info.py
@@ -1,10 +1,17 @@
 import streamlit as st
 
-st.title("Get in Touch!")
-col1, col2 = st.columns([1.5,1], gap="small")
-col2.image('pages/DrMcDonald.jpg', caption="Please direct all questions and concerns to Dr. McDonald, not the developers of this site : )")
+def contact_us():
+    
+    st.title("Get in Touch!", text_alignment = "center")
+    st.divider()
+    col1, col2 = st.columns([1.5,1], gap="small")
+    col2.image('pages/DrMcDonald.jpg', caption="Please direct all questions and concerns to Dr. McDonald, not the developers of this site : )")
 
-col1.subheader(":frowning_man: Dr. Colin McDonald")
-col1.markdown(":email: cmcdona@mcmaster.ca")
-col1.markdown(":classical_building: MDCL 3515/D  ")
-col1.markdown(":telephone_receiver: 905-525-9140 ext. 24131")
+    col1.subheader(":frowning_man: Dr. Colin McDonald")
+    col1.markdown(":email: cmcdona@mcmaster.ca")
+    col1.markdown(":classical_building: MDCL 3515/D  ")
+    col1.markdown(":telephone_receiver: 905-525-9140 ext. 24131")
+
+    col1.write("Disclaimer: Permission has been sought to use this epic photo.")
+
+contact_us()
